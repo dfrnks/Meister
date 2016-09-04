02fb3b920a93c23ae16f55c12d7769d34baf517a
Frnks <douglasfrancardoso@gmail.com>
2016-09-04 14:15:57 -0300

    Liberando CROS para todos

--- skeleton/web/app.php
+++ skeleton/web/app.php
@@ -1,5 +1,12 @@
 <?php
 
+// Allow CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    $headers=getallheaders();
+    @$ACRH=$headers["Access-Control-Request-Headers"];
+    header("Access-Control-Allow-Headers: $ACRH");
+}
+
 require_once __DIR__ . '/../app/bootstrap.php';
 
 $app = new \app\AppInit('DEV');
